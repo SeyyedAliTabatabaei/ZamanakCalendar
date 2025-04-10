434f54e1387e2cde826e4883838c657b36d7839e
SeyyedAliTabatabaei <tabatabaeiali7@gmail.com>
2025-04-10 21:54:29 +0330

    change

--- ZamanakCalendar/src/main/java/seyyed/ali/tabatabaei/zamanakcalendar/MyApp.kt
+++ ZamanakCalendar/src/main/java/seyyed/ali/tabatabaei/zamanakcalendar/MyApp.kt
@@ -1,1 +1,16 @@
-package seyyed.ali.tabatabaei.zamanakcalendarimport android.app.Applicationimport android.content.res.Configurationinternal class MyApp : Application() {    override fun onCreate() {        super.onCreate()        instance = this    }    companion object {        lateinit var instance: MyApp            private set    }}
+package seyyed.ali.tabatabaei.zamanakcalendar
+
+import android.app.Application
+import android.content.res.Configuration
+
+internal class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    companion object {
+        lateinit var instance: MyApp
+            private set
+    }
+}
